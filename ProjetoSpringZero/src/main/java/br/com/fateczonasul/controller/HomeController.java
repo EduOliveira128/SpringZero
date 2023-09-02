2072a3f48014ef4edfbd6fa1fde9b0169f7c327e
EduOliveira128 <eduardo.oliveira128@fatec.sp.gov.br>
2023-09-02 13:34:01 -0300

    Primeira Versao

---
+++ ProjetoSpringZero/src/main/java/br/com/fateczonasul/controller/HomeController.java
@@ -0,0 +1,15 @@
+package br.com.fateczonasul.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
